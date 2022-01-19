998daa20b925346e036bb40f0400ce5caab0f24d
Zhiyu Zhang <zzhang.qppi@gmail.com>
2022-01-19 14:34:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 import docx
 
-def __write_in_txt(word_document, txt_path):
-    for lines in word_document:
-        if lines.text != "附录1 ":
-            lines.clear()
-        else:
-            lines.clear()
-            break
-#删除附录1前的内容（未修改原文档）
-    txt = open(txt_path, mode="w")
-    for lines in word_document:
-        if lines.text != "":
-            txt.write(lines.text+"\n")
-#创建txt并写入附录1中的字符
-    txt.close()
-    return txt
+def __write_in_txt(word_path, txt_path):
+#    for lines in word_document:
+#        if lines.text != "附录1 ":
+#            lines.clear()
+#        else:
+#            lines.clear()
+#            break
+    data = docx.Document(word_path).paragraphs
+    txt = open(txt_path, mode="w+")
+    for lines in data:
+        txt.write(lines.text+"\n")
 
-def data_slicing(source_path, txt_path):
-    data = docx.Document(source_path).paragraphs
-    txt = __write_in_txt(data, txt_path)
-    
+def data_slicing(word_path, txt_path):
+    __write_in_txt(word_path, txt_path)
+    txt = open(txt_path, mode="r")
+    totaldict = {'}f ':[[],[]], '}c ':[[],[]]}
+    for i in txt.readlines():
+        i = i.strip('\n')
+        try:
+            a = i[3:5]
+            b = i[6:-1]
+            totaldict[i[:3]][0].append(a)
+            totaldict[i[:3]][1].append(b)
+        except KeyError:
+            None
+    print(totaldict)
+
 if __name__ == "__main__":
     data_slicing("project.docx","text.txt")
