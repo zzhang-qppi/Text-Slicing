96df9d2cc0e4948f1abd5f5283a56a5ee02073d6
Zhiyu Zhang <zzhang.qppi@gmail.com>
2022-02-28 15:49:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -35,12 +35,14 @@ def data_slicing(word_path, txt_path):
 def data_graphing(word_path, txt_path):
     data = data_slicing(word_path, txt_path)
     fig = plt.figure()
-    fig, axs = plt.subplots(1, len(data), layout='constrained')
+    fig, axs = plt.subplots(len(data), 2, layout='constrained')
     for i in range(0, len(data)):
-        x_data = data[list(data.keys())[i]][0]
-        y_data = data[list(data.keys())[i]][1]
-        axs[i].scatter(x_data, y_data)
-        axs[i].set_title(list(data.keys())[i])
+        data_1 = data[list(data.keys())[i]][0]
+        data_2 = data[list(data.keys())[i]][1]
+        axs[i, 0].scatter(range(0, len(data_1)), data_1, s=5)
+        axs[i, 0].set_title(list(data.keys())[i]+ ' 1')
+        axs[i, 1].scatter(range(0, len(data_2)), data_2, s=5)
+        axs[i, 1].set_title(list(data.keys())[i]+ ' 2')
     return fig
 
 if __name__ == "__main__":
